8b3851d4c002fa9c2a395d8e252f8ddaddcb2648
oleksii.hopchenko@sigma.software <oleksii.hopchenko@sigma.software>
2020-10-05 18:57:45 +0300

    implemented base layout + added root styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HeaderModule} from './modules/header/header.module';
-import {SidebarModule} from './modules/sidebar/sidebar.module';
-import {TasksCatalogModule} from './modules/tasks-catalog/tasks-catalog.module';
+import { HeaderModule } from './modules/header/header.module';
+import { TasksCatalogModule } from './modules/tasks-catalog/tasks-catalog.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HeaderModule,
-    SidebarModule,
     TasksCatalogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
